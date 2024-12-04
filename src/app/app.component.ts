6fc32cc3f242aeaee052b2e1591f023d77429249
SandeepaLakruwan <74184836+SandeepaLakruwan@users.noreply.github.com>
2024-12-04 21:46:32 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { HeaderTopComponent } from "./header-top/header-top.component";
+import { HeaderBottomComponent } from "./header-bottom/header-bottom.component";
+import { FooterComponent } from "./footer/footer.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeaderTopComponent, HeaderBottomComponent, FooterComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'eCom';
